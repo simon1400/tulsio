c985ce63ba281b16ff66fd126531d50f7adbedda
Your Name <>
2021-05-11 11:05:23 +0200

    safgsfg

---
+++ next.config.js
@@ -0,0 +1,29 @@
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} = require('next/constants')
+
+// This uses phases as outlined here: https://nextjs.org/docs/#custom-configuration
+module.exports = (phase) => {
+  // when started in development mode `next dev` or `npm run dev` regardless of the value of STAGING environmental variable
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER
+  // when `next build` or `npm run build` is used
+  const isProd = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== '1'
+  // when `next build` or `npm run build` is used
+  const isStaging = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING === '1'
+
+  console.log(`isDev:${isDev}  isProd:${isProd}   isStaging:${isStaging}`)
+
+  const env = {
+    APP_API: (() => {
+      if (isDev) return 'http://localhost:9001'
+      if (isProd) return 'https://api.tulsio.cz'
+      return 'RESTURL_SPEAKERS:not (isDev,isProd && !isStaging,isProd && isStaging)'
+    })()
+  }
+
+  // next.config.js object
+  return {
+    env,
+  }
+}
